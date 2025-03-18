8f2b1f3d1a09e53cb8b34168399af9f7a27cea92
Anushaa2 <anushagiri545@gmail.com>
2025-03-18 20:54:13 +0545

    few changes

--- setup/base_test.py
+++ setup/base_test.py
@@ -37,19 +37,27 @@ import logging
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+
+# Launch WebDriver with options
+# driver = webdriver.Chrome(options=chrome_options)
 
 class BaseTest:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
     logger = logging.getLogger(__name__)
 
     @pytest.fixture(autouse=True, scope="class")
+
     def setup_method(self,request):
-        driver=webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-notifications")
+        driver=webdriver.Chrome(options=chrome_options)
         self.driver=driver
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
-        cred_path=r"C:\Users\anushagi\OneDrive - Infinite Computer Solutions (India) Limited\Documents\Bhojdeals_automation\creds\cred.json"
+        cred_path=r"E:\Bhojdeals_automation\creds\cred.json"
         with open(cred_path,"r") as file:
             self.cred=json.load(file)
 
