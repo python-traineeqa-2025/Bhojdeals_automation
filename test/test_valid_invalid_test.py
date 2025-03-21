85ee18b6acca43c4bb4a6b42c8712542bdbcd590
anushagi <anushagiri545@gmail.com>
2025-03-21 11:12:34 +0545

    few changes

--- test/test_valid_invalid_test.py
+++ test/test_valid_invalid_test.py
@@ -32,14 +32,16 @@ class TestValidInvalid(BaseTest):
             logging.info(pwd)
 
             login.login_page(email,pwd)
-            invalid_message_email = self.driver.find_element(By.XPATH, "//span[@class='invalid-message']")
 
             try:
+                invalid_message_email = self.driver.find_element(By.XPATH, "//span[@class='invalid-message']")
                 if invalid_message_email:
+
                     logging.info({invalid_message_email.text})
                 else:
-                    invalid_message_password = self.driver.find_element(By.XPATH, "//div[@role='alert']")
-                    logging.info({invalid_message_password.text})
+                    # invalid_message_password = self.driver.find_element(By.XPATH, "//div[@role='alert']")
+                    # logging.info({invalid_message_password.text})
+                    logging.info("The password is incorrect")
 
             except NoSuchElementException:
                 print("The error message doesnot exist")
@@ -57,45 +59,3 @@ class TestValidInvalid(BaseTest):
 
 
 
-
-# import json
-# import logging
-# import time
-# import pytest
-# from selenium.webdriver.common.by import By
-#
-# from page_objects.login.login_page import LoginPage
-# from setup.base_test import BaseTest
-#
-#
-# def load_credentials():
-#     cred_path = r"C:\Users\anushagi\OneDrive - Infinite Computer Solutions (India) Limited\Documents\Bhojdeals_automation\creds\cred_valid_invalid.json"
-#     with open(cred_path, 'r') as file:
-#         cred = json.load(file)
-#
-#     # Convert JSON data to a list of tuples: (test_case_name, email, password)
-#     return [(test_case, details['email'], details['pwd']) for test_case, details in cred.items()]
-#
-#
-# class TestValidInvalid(BaseTest):
-#
-#     @pytest.mark.parametrize("test_case, email, password", load_credentials())
-#     def test_valid_invalid(self, test_case, email, password):
-#         """Parameterized test for login functionality with different credentials."""
-#         invalid_message=self.driver.find_element(By.XPATH,"//span[@class='invalid-message']")
-#         url = self.cred["base_url"]
-#         self.driver.get(url)
-#         time.sleep(3)
-#
-#         login = LoginPage(self.driver)
-#
-#         logging.info(f"Running test: {test_case}")
-#         logging.info({invalid_message.text})
-#         logging.info(f"Email: {email}")
-#         logging.info(f"Password: {password}")
-#
-#         login.login_page(email, password)
-#         time.sleep(5)
-#
-#         self.driver.back()
-#
